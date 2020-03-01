e7ae1b7e884ff70e6bccc5162755ecf7d8ff7b6b
vuongdbrandon <vuong.dbrandon@gmail.com>
2020-03-01 12:15:40 -0500

    added the motors

--- include/subsystemHeaders/globals.hpp
+++ include/subsystemHeaders/globals.hpp
@@ -1,2 +1,11 @@
 //header file for all globals, stuff that we want global access to
 #include "main.h"
+
+//MOTORS
+extern pros::Motor driveRight;
+extern pros::Motor driveLeft;
+extern pros::Motor liftRight;
+extern pros::Motor liftLeft;
+extern pros::Motor intakeRight;
+extern pros::Motor intakeLeft;
+extern pros::Motor tiltTable;
--- src/subsystemFiles/globals.cpp
+++ src/subsystemFiles/globals.cpp
@@ -1,1 +1,10 @@
 #include "main.h"
+
+//MOTORS
+pros::Motor driveRight(10, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_COUNTS); //torque motors
+pros::Motor driveLeft(9, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS); //torque motors
+pros::Motor liftRight(1, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_COUNTS); //torque motors
+pros::Motor liftLeft(2, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS); //torque motors
+pros::Motor intakeRight(3, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS); //standard gear ratio
+pros::Motor intakeLeft(4, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_COUNTS); //standard gear ratio
+pros::Motor tiltTable(8, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS); //torque motors
