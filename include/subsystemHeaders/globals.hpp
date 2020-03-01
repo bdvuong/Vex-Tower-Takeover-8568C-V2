21f9c3ca3c621896ae3583a600bb80b160055d87
vuongdbrandon <vuong.dbrandon@gmail.com>
2020-03-01 12:23:15 -0500

    added controller and adi sensors

--- include/subsystemHeaders/globals.hpp
+++ include/subsystemHeaders/globals.hpp
@@ -1,7 +1,7 @@
 //header file for all globals, stuff that we want global access to
 #include "main.h"
 
-//MOTORS
+//motors
 extern pros::Motor driveRight;
 extern pros::Motor driveLeft;
 extern pros::Motor liftRight;
@@ -9,3 +9,14 @@ extern pros::Motor liftLeft;
 extern pros::Motor intakeRight;
 extern pros::Motor intakeLeft;
 extern pros::Motor tiltTable;
+
+//controller
+extern pros::Controller controller;
+
+//sensors
+extern pros::ADIEncoder trackingH;
+extern pros::ADIEncoder trackingLeft;
+extern pros::ADIEncoder trackingRight;
+extern pros::ADIButton limitSwitch;
+
+//miscellaneous
--- src/subsystemFiles/globals.cpp
+++ src/subsystemFiles/globals.cpp
@@ -1,6 +1,6 @@
 #include "main.h"
 
-//MOTORS
+//motors
 pros::Motor driveRight(10, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_COUNTS); //torque motors
 pros::Motor driveLeft(9, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS); //torque motors
 pros::Motor liftRight(1, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_COUNTS); //torque motors
@@ -8,3 +8,6 @@ pros::Motor liftLeft(2, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_C
 pros::Motor intakeRight(3, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS); //standard gear ratio
 pros::Motor intakeLeft(4, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_COUNTS); //standard gear ratio
 pros::Motor tiltTable(8, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS); //torque motors
+
+//controller
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
